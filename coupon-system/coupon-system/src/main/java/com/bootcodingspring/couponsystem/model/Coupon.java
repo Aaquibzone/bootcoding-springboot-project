aabe05e9143ba1bbedfa830469ac992d9d0d420e
Aaquibzone <codewithaaquib@gmail.com>
2023-08-23 23:40:09 +0530

    Coupan

---
+++ coupon-system/coupon-system/src/main/java/com/bootcodingspring/couponsystem/model/Coupon.java
@@ -0,0 +1,15 @@
+package com.bootcodingspring.couponsystem.model;
+
+
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class Coupon {
+    private String id;
+    private String type;
+    private int validFor;
+
+}
